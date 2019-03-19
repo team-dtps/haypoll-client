53e6123c5a797414c0cc5c11983bb0d9dac7e524
TEONNA ZARAGOZA <teonna.zaragoza@gmail.com>
2019-03-19 15:33:33 -0700

    inital set up and can render app in the browser.

---
+++ src/components/app/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1> HAYHAY </h1>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app/App';
+// import { Provider } from 'react-redux';
+// import store from './store';
+
+render(
+  <App />,
+  document.getElementById('root')
+);
