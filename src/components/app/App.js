8cd062b06a505f760d4daa5a65109da772d03aac
Kate Dameron <katerj33@gmail.com>
2019-03-20 15:19:26 -0700

    updated results comp to show as chart

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PollFormContainer from '../../containers/PollFormConnect';
+import Results from '../results/Results'; 
+
+const results = {
+  prompt: 'prompt',
+  voteTotals: [
+    { choice: 'yay', total: 0 },
+    { choice: 'nay', total: 5 },
+    { choice: 'way', total: 19 },
+    { choice: 'lay', total: 2 },
+    { choice: 'bay', total: 6 }
+  ]
+};
 
 function App() {
   return (
     <>
       <h1> HAYHAY </h1>
       <PollFormContainer />
+      <Results results={results}/>
     </>
   );
 }
