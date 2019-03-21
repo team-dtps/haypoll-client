08ea8853d50e0246ff419d15d8956ef7bd3f58b9
Kristin Hortsch <kristinhortsch@gmail.com>
2019-03-20 17:17:17 -0700

    added some auth0 things, not fully done yet

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PollFormContainer from '../../containers/PollFormConnect';
+import Home from './Home';
 
 function App() {
   return (
     <>
       <h1> HAYHAY </h1>
+      <Home />
       <PollFormContainer />
     </>
   );
